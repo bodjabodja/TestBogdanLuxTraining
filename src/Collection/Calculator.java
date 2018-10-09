df8ab90da4311e5d308f288856caf319a153968c
bodjabodja <bogdan_kryvenko@mail.ru>
2018-10-09 11:32:06 +0300

    new for test

---
+++ src/Collection/Calculator.java
@@ -0,0 +1,19 @@
+package Collection;
+
+/**
+ * Created by dsk16 on 10/9/2018.
+ */
+public class Calculator {
+
+    public  int sum(int a, int b){
+        return a+b;
+    }
+
+    public int devide(int a, int b){
+        if(b==0){
+            throw new IllegalArgumentException();
+        }
+        return a/b;
+    }
+
+}
---
+++ src/Tests/CalculatorTest.java
@@ -0,0 +1,44 @@
+package Tests;
+
+import Collection.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by dsk16 on 10/9/2018.
+ */
+public class CalculatorTest {
+
+    @Test
+    public void testSum(){
+        Calculator calculator = new Calculator();
+        int expected = 5;
+
+        int actual = calculator.sum(2,3);
+
+        //compare
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testDevide(){
+        Calculator calculator = new Calculator();
+        int expected = 10;
+
+        int actual = calculator.devide(20,2);
+
+        //compare
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDevideByZero(){
+        Calculator calculator = new Calculator();
+       // int expected = 10;
+
+        calculator.devide(20,0);
+
+        //compare
+        //Assert.assertEquals(expected,actual);
+    }
+}
